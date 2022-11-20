f2334c143b850fe3016b9f4b4288f9e086c96564
Diego Garcia Curras <d.garciac.2021@alumnos.urjc.es>
2022-11-20 12:40:03 +0100

    Much better

--- encoder.py
+++ encoder.py
@@ -14,6 +14,14 @@ encoder_pin = 16
 
 io.setup(in1_pin, io.OUT)
 io.setup(in2_pin, io.OUT)
+io.setup(button_pin, io.IN, pull_up_down=io.PUD_UP)
+io.setup(encoder_pin, io.IN, pull_up_down=io.PUD_UP)
+
+io.add_event_detect(encoder_pin, io.RISING, callback=callbackEncoder, bouncetime=100)
+
+io.output(in2_pin, False)
+motor = io.PWM(23, 100)
+motor.start(0)
 
 counter = 0
 
@@ -21,21 +29,17 @@ def callbackEncoder(channel):
     global counter
     counter += 1
 
-io.setup(button_pin, io.IN, pull_up_down=io.PUD_UP)
-io.setup(encoder_pin, io.IN, pull_up_down=io.PUD_UP)
-io.add_event_detect(encoder_pin, io.RISING, callback=callbackEncoder, bouncetime=100)
-
-motor = io.PWM(23, 100)
-motor.start(0)
-
-io.output(in2_pin, False)
-
 def callbackExit(signal, frame): # signal and frame when the interrupt was executed.
     io.cleanup() # Clean GPIO resources before exit.
     sys.exit(0)
 
+rpmPrintWD = 0.250
+rpmPrintPrevTime = 0
+
 while True:
     if (not io.input(button_pin)): motor.ChangeDutyCycle(50)
     else: motor.ChangeDutyCycle(0)
-    print(counter)
+    if (time.time() - rpmPrintPrevTime >= rpmPrintWD):
+        rpmPrintPrevTime = time.time()
+        print(counter)
     signal.signal(signal.SIGINT, callbackExit) # callback for CTRL+C
