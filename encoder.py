be2a1dca8100befb6e6664912dc9e07b302a3b3e
Diego Garcia Curras <d.garciac.2021@alumnos.urjc.es>
2022-11-20 16:13:57 +0100

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -1,53 +1,71 @@
 #!/usr/bin/env python3
 
-import RPi.GPIO as io
+import RPi.GPIO as GPIO
 import time
 import signal
 import sys
 
-io.setmode(io.BCM)
+GPIO.setmode(GPIO.BCM)
 
 in1_pin = 23
 in2_pin = 24
 button_pin = 21
 encoder_pin = 16
 
-io.setup(in1_pin, io.OUT)
-io.setup(in2_pin, io.OUT)
-io.setup(button_pin, io.IN, pull_up_down=io.PUD_UP)
-io.setup(encoder_pin, io.IN, pull_up_down=io.PUD_UP)
+GPIO.setup(in1_pin, GPIO.OUT)
+GPIO.setup(in2_pin, GPIO.OUT)
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(encoder_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-io.output(in2_pin, False)
-motor = io.PWM(23, 100)
-motor.start(0)
+GPIO.output(in2_pin, False)
 
-rpmPrintWD = 0.250
-counter = 0
+MOTOR = GPIO.PWM(23, 100)
+MOTOR.start(0)
 
-def callbackEncoder(channel):
-    global counter
-    counter += 1
+RPM_PRINT_TIMER = 0.250
+RPM_PRINT_PREV_TIME = 0
+COUNTER = 0
 
-io.add_event_detect(encoder_pin, io.RISING, callback=callbackEncoder, bouncetime=1)
+def callbackEncoder(channel)
 
-def callbackExit(signal, frame): # signal and frame when the interrupt was executed.
-    io.cleanup() # Clean GPIO resources before exit.
+def callbackExit(signal, frame)
+
+def calculateRPM()
+
+def driveMotor()
+
+def main():
+
+    GPIO.add_event_detect(encoder_pin, GPIO.RISING, callback=callbackEncoder, bouncetime=1)
+
+    print("CTRL + C to exit!", end="\n\n")
+
+    while True:
+        if (time.time() - RPM_PRINT_PREV_TIME >= RPM_PRINT_TIMER):
+            print("                                        ", end="\r")
+            print("RPM: ", int(calculateRPM()), end="\r")
+            COUNTER = 0
+            RPM_PRINT_PREV_TIME = time.time()
+
+        signal.signal(signal.SIGINT, callbackExit) # callback for CTRL+C
+
+callbackEncoder(channel):
+    global COUNTER
+    COUNTER += 1
+
+driveMotor():
+    if (not GPIO.input(button_pin)): MOTOR.ChangeDutyCycle(50)
+    else: MOTOR.ChangeDutyCycle(0)
+
+callbackExit(signal, frame): # signal and frame when the interrupt was executed.
+    GPIO.cleanup() # Clean GPIO resources before exit.
     sys.exit(0)
 
-def calculateRPM():
-    global counter
-    global rpmPrintWD
+calculateRPM():
+    global COUNTER
+    global RPM_PRINT_TIMER
 
-    return (counter * 60) / rpmPrintWD
+    return (COUNTER * 60) / RPM_PRINT_TIMER
 
-rpmPrintPrevTime = 0
-
-while True:
-    if (not io.input(button_pin)): motor.ChangeDutyCycle(50)
-    else: motor.ChangeDutyCycle(0)
-    if (time.time() - rpmPrintPrevTime >= rpmPrintWD):
-        print("                                     ", end="\r")
-        print("RPM: ", int(calculateRPM()), end="\r")
-        counter = 0
-        rpmPrintPrevTime = time.time()
-    signal.signal(signal.SIGINT, callbackExit) # callback for CTRL+C
+if __name__ == "__main__":
+    main()
